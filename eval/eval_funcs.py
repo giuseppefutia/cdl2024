2625ddaa809856cecfd6e8cdd15027db182803c2
Giuseppe Futia <giuseppe.futia@graphaware.com>
2024-11-26 17:57:19 +0100

    Separate node features from edge index in the helper functions for the evaluation

--- eval/eval_funcs.py
+++ eval/eval_funcs.py
@@ -3,13 +3,13 @@ import torch
 def predict(model, data):
     model.eval()
     with torch.no_grad():
-        out = model(data)
+        out = model(data.x, data.edge_index)
         pred = out.argmax(dim=1)
     return pred
 
 def predict_probabilities(model, data):
     model.eval()
     with torch.no_grad():
-        out = model(data)
+        out = model(data.x, data.edge_index)
         probabilities = torch.exp(out)
     return probabilities
